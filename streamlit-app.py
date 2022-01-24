134b1d48f0df14eecfdfc7dadd5825c76a84339b
Rory Hurley <rory.hurley@digital.cabinet-office.gov.uk>
2022-01-24 10:02:14 +0000

    add basic app interface

---
+++ streamlit-app.py
@@ -0,0 +1,6 @@
+# streamlit_app.py
+import spacy_streamlit
+
+models = ["en_core_web_sm", "en_core_web_md"]
+default_text = "Sundar Pichai is the CEO of Google."
+spacy_streamlit.visualize(models, default_text)
